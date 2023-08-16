8dce15d6b69602f9823aafcec5cd2b2b4163d0dc
Jintin <jintin.lin1018@gmail.com>
2023-08-16 12:21:48 +0800

    Fix typo

--- app/src/main/java/com/example/aqisample/data/AqiService.kt
+++ app/src/main/java/com/example/aqisample/data/AqiService.kt
@@ -3,7 +3,7 @@ package com.example.aqisample.data
 import retrofit2.http.GET
 
 interface AqiService {
-    @GET("/api/v2/aqx_p_432?limit=1000&%20api_key=cebebe84-e17d-4022-a28f-81097fda5896&s%20ort=ImportDate%20desc&format=json")
+    @GET("/api/v2/aqx_p_432?limit=1000&api_key=cebebe84-e17d-4022-a28f-81097fda5896&sort=ImportDate%20desc&format=json")
     suspend fun getAqiResult() : AqiRawResult
 
 }
